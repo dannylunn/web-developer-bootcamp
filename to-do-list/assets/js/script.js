eeb3a90865530374850b542cfa56c27cbde1a22a
Danny Lunn <danny.lunn@GL07386M.gamesys.corp>
2019-07-25 09:58:29 +0100

    starting to-do list project

---
+++ to-do-list/assets/js/script.js
@@ -0,0 +1,19 @@
+$("ui").on("click", "li", function(){
+  $(this).toggleClass("strikethrough");
+  console.log('clicked');
+});
+
+$("li span").on("click", function(event){
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  })
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var input = $(this).val();
+    $("ul").append("<li><span>X </span>" + input + "</li>");
+    $(this).val("");
+  };
+});
